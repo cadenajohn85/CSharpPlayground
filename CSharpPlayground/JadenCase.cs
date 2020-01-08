830b308070007b4c0acbbde99a776a34eeca6660
John Cadena <cadena.john.85@gmail.com>
2020-01-08 07:00:17 -0600

    * CSharpPlayground.csproj: complete "JadenCase" kata
    * Program.cs:
    * JadenCase.cs:

---
+++ CSharpPlayground/JadenCase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CSharpPlayground
+{
+    public static class JadenCase
+    {
+        public static string ToJadenCase(this string phrase)
+        {
+            string[] words = phrase.Split();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+                Console.WriteLine(words[i]);
+            }
+            return String.Join(" ", words);
+        }
+
+        public static void Main(string[] args)
+        {
+            ToJadenCase("blah di dah");
+        }
+    }
+}
--- CSharpPlayground/Program.cs
+++ CSharpPlayground/Program.cs
@@ -1,27 +1,28 @@
-﻿using System;
+﻿//using System;
 
-namespace CSharpPlayground
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            string message = "Let's learn C#";
-            Console.WriteLine("Idea: " + message);
-            message = "This is important for my job.";
-            Console.WriteLine($"Reason: {message}");
-            String name = "John";
-            String cohort = "Ceres";
-            Console.WriteLine($"Hello, I'm {name} from the {cohort} cohort.");
-            if (name.Length == cohort.Length)
-            {
-                Console.WriteLine($"{name} has more letters than {cohort}");
-            } else
-            {
-                Console.WriteLine($"{name} does not have more letters than {cohort}");
-            }
-            string defaultMessage = "Hello World";
-            Console.WriteLine(defaultMessage.Replace("Hello", "Greetings"));
-        }
-    }
-}
+//namespace CSharpPlayground
+//{
+//    class MainClass
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string message = "Let's learn C#";
+//            Console.WriteLine("Idea: " + message);
+//            message = "This is important for my job.";
+//            Console.WriteLine($"Reason: {message}");
+//            String name = "John";
+//            String cohort = "Ceres";
+//            Console.WriteLine($"Hello, I'm {name} from the {cohort} cohort.");
+//            if (name.Length == cohort.Length)
+//            {
+//                Console.WriteLine($"{name} has more letters than {cohort}");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"{name} does not have more letters than {cohort}");
+//            }
+//            string defaultMessage = "Hello World";
+//            Console.WriteLine(defaultMessage.Replace("Hello", "Greetings"));
+//        }
+//    }
+//}
